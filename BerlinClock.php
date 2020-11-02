d6568428118e0b7e0c9181978834bc1e801eef62
Maxim Thomas <maxim.thomas.be@gmail.com>
2020-11-02 15:04:01 +0100

    refactor - extract minuteToString

--- BerlinClock.php
+++ BerlinClock.php
@@ -5,12 +5,8 @@ class BerlinClock{
     {
         $tabTime = str_split($message);
         $minutes = $this->giveMinute($tabTime);
+        return $this->minuteToString($minutes);
 
-        if($minutes % 5 == 1) return "OXXX";
-        if($minutes % 5 == 2) return "OOXX";
-        if($minutes % 5 == 3) return "OOOX";
-        if($minutes % 5 == 4) return "OOOO";
-        return "XXXX";
     }
 
     public function giveMinute(Array $tabTime) : int
@@ -18,6 +14,15 @@ class BerlinClock{
         $tens = (int) $tabTime[3];
         $unit = (int) $tabTime[4];
         return ($tens*10) + $unit;
+    }
+
+    public function minuteToString($minutes) :string
+    {
+        if($minutes % 5 == 1) return "OXXX";
+        if($minutes % 5 == 2) return "OOXX";
+        if($minutes % 5 == 3) return "OOOX";
+        if($minutes % 5 == 4) return "OOOO";
+        return "XXXX";
     }
 
 
